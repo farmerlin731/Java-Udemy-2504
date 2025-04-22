42fcf173d29d69e963b393496c8549dbb75a90c3
farmerlin731 <69831413+farmerlin731@users.noreply.github.com>
2025-04-22 18:01:00 +0800

    Create the canvas.

--- java-snake-game/src/com/company/Main.java
+++ java-snake-game/src/com/company/Main.java
@@ -1,4 +1,30 @@
 package com.company;
 
-public class Main {
+import javax.swing.*;
+import java.awt.*;
+
+
+public class Main extends JPanel {
+
+    public static int width = 400;
+    public static int height = 400;
+
+    public Main() {
+
+    }
+
+    @Override
+    public Dimension getPreferredSize() {
+        return new Dimension(width,height);
+    }
+
+    public static void main(String[] args) {
+        JFrame window = new JFrame("Snake Game");
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setContentPane(new Main());
+        window.pack();
+        window.setLocationRelativeTo(null);
+        window.setVisible(true);
+        window.setResizable(false);
+    }
 }
