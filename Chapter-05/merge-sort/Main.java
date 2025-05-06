82846015647b240a5ce59c5a6f2b5ec635ff6d78
farmerlin731 <69831413+farmerlin731@users.noreply.github.com>
2025-05-06 15:18:57 +0800

    Merge-sort example.

---
+++ Chapter-05/merge-sort/Main.java
@@ -0,0 +1,60 @@
+class MergeSort {
+    public static void mergeSort(int[] list) {
+        if (list.length > 1) {
+            // merge sort the first half
+            int[] firstHalf = new int[list.length / 2];
+            System.arraycopy(list, 0, firstHalf, 0, list.length / 2);
+            mergeSort(firstHalf);
+
+            // merge sort the second half
+            int secondHalfLength = list.length - list.length / 2;
+            int[] secondHalf = new int[secondHalfLength];
+            System.arraycopy(list, list.length / 2, secondHalf, 0, secondHalfLength);
+            mergeSort(secondHalf);
+
+            // merge
+            merge(firstHalf, secondHalf, list);
+        }
+    }
+
+
+    public static void merge(int[] list1, int[] list2, int[] temp) {
+        int i = 0;
+        int j = 0;
+        int k = 0;
+
+        while (i < list1.length && j < list2.length) {
+            if (list1[i] < list2[j]) {
+                temp[k] = list1[i];
+                i++;
+            } else {
+                temp[k] = list2[j];
+                j++;
+            }
+            k++;
+        }
+
+        while (i < list1.length) {
+            temp[k] = list1[i];
+            k++;
+            i++;
+        }
+        while (j < list2.length) {
+            temp[k] = list2[j];
+            k++;
+            j++;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] list = {4, 3, 2, 7, 6, -21, 14, 5, 6, 7, 899, 0, 4, 3, 322, 1, 5};
+        mergeSort(list);
+        for (int n : list) {
+            System.out.print(n + " ");
+        }
+    }
+}
+
+public class Main {
+
+}
