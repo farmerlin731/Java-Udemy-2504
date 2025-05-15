2b59bf1b92898fd7deccb0a6c9288490747c9d5a
farmerlin731 <69831413+farmerlin731@users.noreply.github.com>
2025-05-15 15:57:10 +0800

    Second example.

--- Chapter-06/01-lambda-example/Main.java
+++ Chapter-06/01-lambda-example/Main.java
@@ -1,11 +1,18 @@
-interface Add5{
+import java.util.Arrays;
+
+interface Add5 {
     int add5(int x);
 }
 
-public class Main{
-    public static void main(String[] args){
+public class Main {
+    public static void main(String[] args) {
         System.out.println("hello world:)");
-        Add5 myAdd5Obj = x -> x+5;
+        Add5 myAdd5Obj = x -> x + 5;
         System.out.println(myAdd5Obj.add5(10));
+
+        int[] array = Arrays.stream(new int[]{1, 2, 3, 4, 5}).map(a -> a * 5).toArray();
+        for (int e : array) {
+            System.out.println(e);
+        }
     }
 }
