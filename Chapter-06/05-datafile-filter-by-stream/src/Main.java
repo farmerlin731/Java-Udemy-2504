43f315499623cb806ed3809b79409e69eb77dcfb
farmerlin731 <69831413+farmerlin731@users.noreply.github.com>
2025-05-17 18:41:39 +0800

    Example of hash-map.

--- Chapter-06/05-datafile-filter-by-stream/src/Main.java
+++ Chapter-06/05-datafile-filter-by-stream/src/Main.java
@@ -9,6 +9,7 @@ import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collector;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -19,11 +20,17 @@ public class Main {
         //Example1
         Stream<String> rows = Files.lines(Paths.get("data.txt"), StandardCharsets.UTF_8);
 
-        rows.map(x -> x.split(","))
+        Map<String, Integer> map = rows.map(x -> x.split(","))
                 .filter(x -> x.length == 3)
-                .filter(x->Integer.parseInt(x[1])>15)
-                .forEach(x-> System.out.println(String.join(",",x)));
+                .filter(x -> Integer.parseInt(x[1]) > 15)
+                .collect(Collectors.toMap(
+                        x -> x[0],
+                        x -> Integer.parseInt(x[1])
+                ));
 
+        map.forEach((key, value) -> {
+            System.out.println("Key:" + key + ", and the value is:" + value);
+        });
         rows.close();
     }
 }
